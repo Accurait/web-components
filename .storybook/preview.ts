c1d2cd5feaee601fb9fad70a76062191b7e55c23
João Pedro Antunes Silva <joao-pedroas@hotmail.com>
2023-05-09 00:04:05 -0300

    chore(storybook): use typescript

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,13 +1,13 @@
-/** @type { import('@storybook/vue3').Preview } */
+import type { Preview } from '@storybook/vue3'
 
-/** Storybook is not able to find the tailwind css file automatically
-    so we need to import it manually and it is generated from the tailwind-input.css and conten of the tailwind.config.js
+/** Storybook is not able to find the tailwind css file automatically,
+    so we need to import it manually, and it is generated from the tailwind-input.css and content of the tailwind.config.js
 */
 import '../assets/css/tailwind-output.css'
-import { action } from '@storybook/addon-actions'
+// import { action } from '@storybook/addon-actions'
 import { setup } from '@storybook/vue3'
 
-const preview = {
+const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {

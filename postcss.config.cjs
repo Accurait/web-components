81ff4489285be23da650e12c5b00ea39918eb55c
João Pedro Antunes Silva <joao-pedroas@hotmail.com>
2023-05-09 21:29:46 -0300

    chore: configure postcss

---
+++ postcss.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  plugins: {
+    'postcss-url': {},
+    'postcss-import': {},
+    'postcss-hexrgba': {},
+    'postcss-nesting': {},
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}

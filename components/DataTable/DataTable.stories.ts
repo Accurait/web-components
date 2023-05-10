452cfe726ee27ce1a1a2393ebc796ea54b34afe8
João Pedro Antunes Silva <joao-pedroas@hotmail.com>
2023-05-09 22:17:29 -0300

    feat(components): add `DataTable` story (wip)
    It is using example from PrimeVue

--- components/DataTable/DataTable.stories.ts
+++ components/DataTable/DataTable.stories.ts
@@ -1,15 +1,36 @@
 import DataTable from './DataTable.vue'
+import DataTableColumn from './DataTableColumn.vue'
 import type { Meta, StoryObj } from '@storybook/vue3'
 
 const meta: Meta<typeof DataTable> = {
   title: 'DataTable',
   component: DataTable,
   render: (args) => ({
-    components: { DataTable },
-    template: '<DataTable v-bind="args"></DataTable>',
+    components: { DataTable, DataTableColumn },
+    template:
+      '<DataTable>' +
+      '    <DataTableColumn field="code" header="Code" />\n' +
+      '    <DataTableColumn field="name" header="Name" />\n' +
+      '    <DataTableColumn field="category" header="Category" />\n' +
+      '    <DataTableColumn field="quantity" header="Quantity" />\n' +
+      '</DataTable>',
   }),
   args: {
-    //
+    value: [
+      {
+        id: '1000',
+        code: 'f230fh0g3',
+        name: 'Bamboo Watch',
+        description: 'Product Description',
+        image: 'bamboo-watch.jpg',
+        price: 65,
+        category: 'Accessories',
+        quantity: 24,
+        inventoryStatus: 'INSTOCK',
+        rating: 5,
+      },
+    ],
+    tableStyle: 'min-width: 50rem',
   },
 }
 

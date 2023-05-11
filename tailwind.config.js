2f29890989d6f58351a2eee32803fd0d1c92bea9
João Pedro Antunes Silva <joao-pedroas@hotmail.com>
2023-05-10 21:49:24 -0300

    chore(tailwind): add stories and mdx

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './components/**/*.{js,vue,ts}',
+    './components/**/*.{js,vue,ts,mdx}',
     './layouts/**/*.vue',
     './pages/**/*.vue',
     './plugins/**/*.{js,ts}',
     './nuxt.config.{js,ts}',
     './node_modules/flowbite.{js,ts}',
+    './stories/**/*.{js,vue,ts,mdx}',
   ],
   theme: {
     extend: {},

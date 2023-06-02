7c71d436f2027cbc3697e24171f5bad3c95afc45
João Pedro Antunes Silva <joao-pedroas@hotmail.com>
2023-06-01 22:24:33 -0300

    feat(formkit): basic configuration

--- formkit.config.ts
+++ formkit.config.ts
@@ -1,5 +1,13 @@
-import type { DefaultConfigOptions } from '@formkit/vue'
+import { type DefaultConfigOptions } from '@formkit/vue'
+import { generateClasses } from '@formkit/themes'
+import { createMultiStepPlugin } from '@formkit/addons'
+import '@formkit/addons/css/multistep'
+import { createLazyPlugin, createLoadingPlugin } from './formkit/plugins'
+import theme from './formkit.theme'
 
 export default <DefaultConfigOptions>{
-  //
+  config: {
+    classes: generateClasses(theme),
+  },
+  plugins: [createMultiStepPlugin(), createLazyPlugin(), createLoadingPlugin()],
 }
